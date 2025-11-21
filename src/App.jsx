85d1c98f7b315df9693c79a7ed1f15f5b4e2ed79
HyeongjinKim98 <201721157@sangmyung.kr>
2025-11-21 16:18:56 +0900

    feat :  로그인 상태에 따른 라우팅처리

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthStore } from "./stores/authStore";
 import { SignUp } from "./SignUp/SignUp";
 import { Login } from "./Login/Login";
 import { Todo } from "./Todo/Todo";
 const App = () => {
   return (
     <>
-      {/* <Todo/>*/}
-      <SignUp/>
-      <Login/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Root/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/Signup" element={<SignUp/>}/>
+          <Route path="/todo" element={<Todo/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 };
+
+const Root =()=>{
+  const user = useAuthStore(state=>state.user);
+
+  if(user) {
+    return <Navigate to="/todo"/>
+  }
+  return <Navigate to="/login"/>
+}
 
 export default App;
