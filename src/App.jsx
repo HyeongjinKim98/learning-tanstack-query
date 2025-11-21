b9e226caafdaabd751428b3fad34d1321e7de751
HyeongjinKim98 <201721157@sangmyung.kr>
2025-11-21 19:51:01 +0900

    feat : todo service에 protected route 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useAuthStore } from "./stores/authStore";
+import { ProtectedRoute } from "./Router/ProtectedRoute";
 import { SignUp } from "./SignUp/SignUp";
 import { Login } from "./Login/Login";
 import { Todo } from "./Todo/Todo";
@@ -9,22 +10,29 @@ const App = () => {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Root/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/Signup" element={<SignUp/>}/>
-          <Route path="/todo" element={<Todo/>}/>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/Signup" element={<SignUp />} />
+          <Route
+            path="/todo"
+            element={
+              <ProtectedRoute>
+                <Todo />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 };
 
-const Root =()=>{
-  const user = useAuthStore(state=>state.user);
-  if(user) {
-    return <Navigate to="/todo"/>
+const Root = () => {
+  const user = useAuthStore((state) => state.user);
+  if (user) {
+    return <Navigate to="/todo" />;
   }
-  return <Navigate to="/login"/>
-}
+  return <Navigate to="/login" />;
+};
 
 export default App;
